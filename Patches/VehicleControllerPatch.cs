dfbc1af5b5179b533680b06e6255f2d7517c8ad9
Yohomok <103838479+ScandalTheVandal@users.noreply.github.com>
2026-07-10 13:17:05 +0100

    Update VehicleControllerPatch.cs

--- Patches/VehicleControllerPatch.cs
+++ Patches/VehicleControllerPatch.cs
@@ -3,11 +3,11 @@ using HarmonyLib;
 namespace CompanyCruiserConfig.Patches;
 
 [HarmonyPatch(typeof(VehicleController))]
-internal class VehicleControllerPatch
+public static class VehicleControllerPatch
 {
     [HarmonyPatch(nameof(VehicleController.Start))]
     [HarmonyPrefix]
-    private static void StartPrefix(VehicleController __instance)
+    private static void Start_Prefix(VehicleController __instance)
     {
         __instance.baseCarHP = CompanyCruiserConfig.baseCarHP.Value;
     }
@@ -16,26 +16,26 @@ internal class VehicleControllerPatch
     [HarmonyPostfix]
     private static void StartPostfix(VehicleController __instance) 
     {
-        __instance.brakeSpeed = CompanyCruiserConfig.brakeSpeed.Value;
+        //__instance.brakeSpeed = CompanyCruiserConfig.brakeSpeed.Value; //unused
         __instance.carAcceleration = CompanyCruiserConfig.carAcceleration.Value;
-        __instance.carFragility = CompanyCruiserConfig.carFragility.Value;
+        //__instance.carFragility = CompanyCruiserConfig.carFragility.Value; //unused
         __instance.carHitPlayerForceFraction = CompanyCruiserConfig.carHitPlayerForceFraction.Value;
-        __instance.carMaxSpeed = CompanyCruiserConfig.carMaxSpeed.Value;
+        //__instance.carMaxSpeed = CompanyCruiserConfig.carMaxSpeed.Value; //unused
         __instance.carReactToPlayerHitMultiplier = CompanyCruiserConfig.carReactToPlayerHitMultiplier.Value;
-        __instance.engineIntensityPercentage = CompanyCruiserConfig.engineIntensityPercentage.Value;
+        //__instance.engineIntensityPercentage = CompanyCruiserConfig.engineIntensityPercentage.Value; //no point changing this, this is for audio effects
         __instance.EngineTorque = CompanyCruiserConfig.engineTorque.Value;
         __instance.idleSpeed = CompanyCruiserConfig.idleSpeed.Value;
         __instance.jumpForce = CompanyCruiserConfig.jumpForce.Value;
-        __instance.MaxEngineRPM = CompanyCruiserConfig.maxEngineRPM.Value;
+        //__instance.MaxEngineRPM = CompanyCruiserConfig.maxEngineRPM.Value; //unused
         __instance.maximumBumpForce = CompanyCruiserConfig.maximumBumpForce.Value;
         __instance.mediumBumpForce = CompanyCruiserConfig.mediumBumpForce.Value;
-        __instance.MinEngineRPM = CompanyCruiserConfig.minEngineRPM.Value;
+        //__instance.MinEngineRPM = CompanyCruiserConfig.minEngineRPM.Value; //unused
         __instance.minimalBumpForce = CompanyCruiserConfig.minimalBumpForce.Value;
         __instance.movingAverageLength = CompanyCruiserConfig.movingAverageLength.Value;
         __instance.pushForceMultiplier = CompanyCruiserConfig.pushForceMultiplier.Value;
         __instance.pushVerticalOffsetAmount = CompanyCruiserConfig.pushVerticalOffsetAmount.Value;
-        __instance.radioSignalDecreaseThreshold = CompanyCruiserConfig.radioSignalDecreaseThreshold.Value;
-        __instance.radioSignalQuality = CompanyCruiserConfig.radioSignalQuality.Value;
+        //__instance.radioSignalDecreaseThreshold = CompanyCruiserConfig.radioSignalDecreaseThreshold.Value; //gets changed everytime you tune the radio
+        //__instance.radioSignalQuality = CompanyCruiserConfig.radioSignalQuality.Value; //gets changed everytime you tune the radio
         __instance.radioSignalTurbulence = CompanyCruiserConfig.radioSignalTurbulence.Value;
         __instance.speed = CompanyCruiserConfig.speed.Value;
         __instance.springForce = CompanyCruiserConfig.springForce.Value;
